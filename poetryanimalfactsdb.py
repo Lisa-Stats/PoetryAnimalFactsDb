6f1e4dd435d8199dd6dc86c14c8b96346cc2c6d9
Lisa Stats <lisa.stathopulos@gmail.com>
2023-11-14 12:16:46 -0500

    Initial commit

---
+++ poetryanimalfactsdb.py
@@ -0,0 +1,68 @@
+import requests
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@localhost:5432/postgres"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Fact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    animal = db.Column(db.String(10))
+    fact = db.Column(db.Text())
+
+def cat_request():
+    cat = requests.get("https://meowfacts.herokuapp.com")
+    cat_json = cat.json()
+    cat_data = cat_json["data"]
+    [single_fact] = cat_data
+    with app.app_context():
+        fact = Fact(
+            animal="cat",
+            fact=single_fact
+        )
+        db.session.add(fact)
+        db.session.commit()
+    return single_fact
+
+def cat_list():
+    with app.app_context():
+        cat_facts = Fact.query.order_by(Fact.id.desc()).filter_by(animal="cat").limit(3)
+    return cat_facts
+
+def dog_request():
+    dog = requests.get("https://dog-api.kinduff.com/api/facts")
+    dog_json = dog.json()
+    dog_data = dog_json["facts"]
+    [single_fact] = dog_data
+    with app.app_context():
+        fact = Fact(
+            animal="dog",
+            fact=single_fact
+        )
+        db.session.add(fact)
+        db.session.commit()
+    return single_fact
+
+def dog_list():
+    with app.app_context():
+        dog_facts = Fact.query.order_by(Fact.id.desc()).filter_by(animal="dog").limit(3)
+    return dog_facts
+
+@app.route("/")
+def main():
+    if request.args.get("dog", False) == "Get Dog Fact":
+        return render_template("index.html",
+                               dog_data=dog_request(), dog_facts=dog_list())
+    elif request.args.get("cat", False) == "Get Cat Fact":
+        return render_template("index.html",
+                               cat_data=cat_request(), cat_facts=cat_list())
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
