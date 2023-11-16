1481d4f94d2c34327886698e4bf5d52014a29862
Lisa Stats <lisa.stathopulos@gmail.com>
2023-11-16 10:09:02 -0500

    Remove init file + remove flask_migrate

--- poetryanimalfactsdb.py
+++ poetryanimalfactsdb.py
@@ -1,14 +1,12 @@
 import requests
 from flask import Flask, render_template, request
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@localhost:5432/postgres"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@postgresql:5432/postgres"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 class Fact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -65,4 +63,6 @@ def main():
         return render_template("index.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+      db.create_all()
     app.run(debug=True, host="0.0.0.0")
